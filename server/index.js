b2c6d0185347381763a7e868b642f762314b989d
AhmadMohSal <ahmad.moh.sal@gmail.com>
2022-09-11 18:05:16 +0300

    routes for reg,login

--- server/index.js
+++ server/index.js
@@ -8,8 +8,15 @@ app.use(express.json());
 app.use(cors());
 
 //routes
+const registerRouter = require("./routes/register");
+const loginRouter = require("./routes/login");
+
 
 //.use
+app.use("/register", registerRouter);
+app.use("/login", loginRouter);
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
